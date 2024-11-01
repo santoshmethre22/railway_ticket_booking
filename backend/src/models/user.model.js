a3feacd78372f01173a7bfc559b535b34e5dfbfb
santoshmethre22 <santoshmethre22@gmail.com>
2024-11-01 22:27:36 +0530

    update user.model.js

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -45,6 +45,47 @@ const userSchema= new mongoose.Schema({
 /// --------------------------------------------->
 
 // complete the code wit the refferece from user and video model of the backend
+userSchema.pre("save",async function(next){ /// in this we have passed the function not arrow function 
+    // because arrow func cant access the class members --- this.password , so used the function 
+  if(!this.isModified("password")) return next();
+
+        this.password=await bcrypt.hash(this.password,10);
+        next();
+})
+
+userSchema.method.isPasswordCorrect=async function (password){
+    return await bcrypt.compare(password,this.password);
+}
+
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 
 export  const User=mongoose.model("User",userSchema);
